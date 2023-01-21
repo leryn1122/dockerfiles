62945c0a86dc0ad2435122c2e3e42bdc43aeeafd
leryn <pinktin@sina.com>
2023-01-21 16:03:30 +0800

    Modify dockerfile template

---
+++ s3fs/docker-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eux
+
+cd $(dirname ${BASH_SOURCE[0]}) && pwd || exit 1
+[[ -f "../scripts/lib.sh" ]] && source "../scripts/lib.sh"
+
+export DEBIAN_VERSION=11.6-slim
+export S3FS_VERSION=1.89
+export MIRRORS_SOURCE=mirrors.tuna.tsinghua.edu.cn
+
+write_dockerignore
+
+docker build \
+  --tag harbor.leryn.top/infra/s3fs:v${S3FS_VERSION} \
+  --build-arg DEBIAN_VERSION=${DEBIAN_VERSION} \
+  --build-arg MIRRORS_SOURCE=${MIRRORS_SOURCE} \
+  --build-arg S3FS_VERSION=${S3FS_VERSION}-1 \
+  .
+
+remove_dockerignore
---
+++ scripts/lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function write_dockerignore {  
+  cat <<EOF> .dockerignore
+docker-build.sh
+EOF
+}
+
+function remove_dockerignore {
+  rm ./.dockerignore
+}
